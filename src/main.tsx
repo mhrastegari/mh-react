3fba82f5935a826055a9281812ffdef922c9f32c
Mohammad Hosein Rastegarinia <mh.rastegari@outlook.com>
2024-10-12 11:13:35 +0330

    Fix serviceWorker path

--- src/main.tsx
+++ src/main.tsx
@@ -5,7 +5,7 @@ import './index.css'
 
 if ('serviceWorker' in navigator) {
   window.addEventListener('load', () => {
-    navigator.serviceWorker.register('/serviceWorker.js')
+    navigator.serviceWorker.register('./serviceWorker.js')
       .then((registration) => {
         console.log('ServiceWorker registered: ', registration);
       })
